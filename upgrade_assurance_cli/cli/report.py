46bf98e6d27795e47a28b981b3896bddc27d85c8
Adam Baumeister <abaumeister@paloaltonetworks.com>
2026-03-19 12:25:02 +1100

    squash: Snapshot comparison output

--- upgrade_assurance_cli/cli/report.py
+++ upgrade_assurance_cli/cli/report.py
@@ -7,26 +7,66 @@ import sys
 from rich.table import Table
 from panos_upgrade_assurance.snapshot_compare import SnapshotCompare
 
+
 class ReportTypeEnum(str, enum.Enum):
     readiness = "readiness"
     snapshot = "snapshotr"
 
+
 class BadDeviceStringException(Exception):
     pass
 
-class CheckReport:
+
+class SnapshotReport:
+    def __init__(self, device, report, timestamp):
+        self.timestamp = int(timestamp)
+        self.datetime = datetime.datetime.fromtimestamp(self.timestamp)
+        self.device = device
+        self.report = report
+        self.report_type = ReportTypeEnum.snapshot
+
+    @property
+    def count_failed_checks(self) -> int:
+        return len([k for k, v in self.report.items() if not v.get("passed")])
+
+    @property
+    def count_passed_checks(self) -> int:
+        return len([k for k, v in self.report.items() if v.get("passed")])
+
+    def checks_as_table(self):
+        table = [
+            [
+                "check_name",
+                "passed",
+                "missing",
+                "added",
+                "count_change_percentage"
+            ]
+        ]
+        for check_name, check_value in self.report.items():
+            row = [check_name]
+            for i in table[0][1:]:
+                result = check_value.get(i)
+                if isinstance(result, dict):
+                    result = result.get("passed")
+                row.append(result)
+            table.append(row)
+
+        return table
+
+
+class ReadinessCheckReport:
     def __init__(
             self,
             device: str,
             report: dict[str, dict],
             timestamp: int | str,
-            report_type: ReportTypeEnum = ReportTypeEnum.readiness
     ):
         self.timestamp = int(timestamp)
         self.datetime = datetime.datetime.fromtimestamp(self.timestamp)
         self.device = device
         self.report = report
-        self.report_type = report_type
+        self.report_type = ReportTypeEnum.readiness
 
     @property
     def count_failed_checks(self) -> int:
@@ -56,17 +96,9 @@ class CheckReport:
             ]
         ]
         for k, check in self.report.items():
-            # If this is a snapshot test
-
-            if "state" not in check:
-                # If this is a snapshot comparison
-                state = check.get("passed")
-                reason = f"Failed: {self.calc_change_reason_from_snapshot_report(check)}"
-                status = ""
-            else:
-                state = check["state"]
-                status = check["status"]
-                reason = check["reason"]
+            state = check["state"]
+            status = check["status"]
+            reason = check["reason"]
 
             table.append([
                 k,
@@ -76,20 +108,27 @@ class CheckReport:
             ])
         return table
 
+
 class CheckReports:
     def __init__(self):
-        self.reports: list[CheckReport] = []
+        self.readiness_reports: list[ReadinessCheckReport] = []
+        self.snapshot_reports: list[SnapshotReport] = []
 
-    def add_report(self, report: CheckReport):
-        self.reports.append(report)
+    def add_readiness_report(self, report: ReadinessCheckReport):
+        self.readiness_reports.append(report)
+
+    def add_snapshot_report(self, report: SnapshotReport):
+        self.snapshot_reports.append(report)
 
     @property
     def failed_reports(self):
-        return [r for r in self.reports if r.count_failed_checks > 0]
+        return [
+            r for r in self.readiness_reports + self.snapshot_reports if r.count_failed_checks > 0
+        ]
 
     @property
     def passed_reports(self):
-        return [r for r in self.reports if r.count_failed_checks == 0]
+        return [r for r in self.readiness_reports + self.snapshot_reports if r.count_failed_checks == 0]
 
     def exit_by_status(self):
         if self.failed_reports:
@@ -110,7 +149,7 @@ class CheckReports:
             table.add_column(c)
 
         for r in list_table[1:]:
-            failed_count = r[2]
+            failed_count = r[3]
             style = "green"
             if failed_count > 0:
                 style = "bold red"
@@ -119,10 +158,15 @@ class CheckReports:
 
         return table
 
+    @property
+    def reports(self):
+        return self.snapshot_reports + self.readiness_reports
+
     def counts_as_table(self) -> list[list[str | int]]:
         table = [
             [
                 "timestamp",
+                "report_type",
                 "device",
                 "failed_checks_count",
                 "passed_checks_count"
@@ -132,6 +176,7 @@ class CheckReports:
             table.append(
                 [
                     report.datetime.isoformat(),
+                    report.report_type.value,
                     report.device,
                     report.count_failed_checks,
                     report.count_passed_checks
@@ -139,17 +184,18 @@ class CheckReports:
             )
         return table
 
-    def get_latest_report_by_device(self, device_str: str):
-        sorted_devices = sorted([d for d in self.reports if d.device == device_str], key=lambda d: d.timestamp, reverse=True)
+    def get_latest_report_by_device(self, device_str: str, reports: list):
+        sorted_devices = sorted([d for d in reports if d.device == device_str], key=lambda d: d.timestamp,
+                                reverse=True)
         if not sorted_devices:
-            raise BadDeviceStringException(f"device {device_str} not found.")
+            raise BadDeviceStringException(f"device {device_str} not found or no report was present.")
 
         return sorted_devices[0]
 
-    def device_report_as_rich_table(self, device_str: str):
-        report = self.get_latest_report_by_device(device_str)
+    def device_readiness_report_as_rich_table(self, device_str: str):
+        report = self.get_latest_report_by_device(device_str, self.readiness_reports)
         list_table = report.checks_as_table()
-        table = Table(caption=f"Checks were ran at {report.datetime.isoformat()}")
+        table = Table(caption=f"READINESS Checks were ran at {report.datetime.isoformat()}")
         for c in list_table[0]:
             table.add_column(c)
 
@@ -162,11 +208,29 @@ class CheckReports:
 
         return table
 
+    def device_snapshot_report_as_rich_table(self, device_str: str):
+        report = self.get_latest_report_by_device(device_str, self.snapshot_reports)
+        table = Table(caption=f"SNAPSHOT Report was ran at {report.datetime.isoformat()}")
+        table_list = report.checks_as_table()
+        for header in table_list[0]:
+            table.add_column(f"{header}")
+
+        for r in table_list[1:]:
+            style = "green"
+            if not r[1]:
+                style = "bold red"
+            new_row_items = [str(i) for i in r]
+
+            table.add_row(*new_row_items, style=style)
+
+        return table
+
 def details_from_filename(filename: str) -> tuple[str, str, str]:
     """Gets the device name, the check type and the timestamp based on the filename."""
     filename = filename.replace(".json", "")
     check_type, device, timestamp = filename.split("_")
     return check_type, device, timestamp
+
 
 def generate_reports_from_store(store_path: pathlib.Path):
     """Returns the completed readiness check report based on the generated check results in
@@ -177,12 +241,19 @@ def generate_reports_from_store(store_path: pathlib.Path):
     for file in store_path.iterdir():
         if file.is_file() and file.suffix == '.json':
             check_type, device, timestamp = details_from_filename(file.name)
-            report = CheckReport(
-                timestamp=timestamp,
-                device=device,
-                report=json.load(open(file)),
-                report_type=ReportTypeEnum(check_type)
-            )
-            reports.add_report(report)
+            if check_type == ReportTypeEnum.readiness.value:
+                report = ReadinessCheckReport(
+                    timestamp=timestamp,
+                    device=device,
+                    report=json.load(open(file)),
+                )
+                reports.add_readiness_report(report)
+            elif check_type == ReportTypeEnum.snapshot:
+                report = SnapshotReport(
+                    timestamp=timestamp,
+                    device=device,
+                    report=json.load(open(file)),
+                )
+                reports.add_snapshot_report(report)
 
     return reports
